b2234b1bd2577ff044fcd23c8b2a787fe7461c88
MuhammadJunaid01 <m.junaidbkh2020@gmail.com>
2022-01-30 20:34:39 +0600

    first array mapin reactnative app

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+
+const friends=[
+  {name:"Md Sohel", Occupation:"Backend Developer ", Address:"Bogoda"},
+  {name:"Md Zahidul Haque", Occupation:"Front End Developer ", Address:"Developer"},
+  {name:"MD Mahin", Occupation:"Front End Developer ", Address:"Dhaka"},
+  {name:"Md Junaid", Occupation:"Backend Developer ", Address:"Chittagong"},
+]
 export default function App() {
+ const list=()=>{
+   
+  return   friends.map((friend,index)=>{
+    return (
+      <View key={index}>
+      <Text style={{color:"white",marginBottom:10}}>{friend.name}</Text>
+    </View>
+     )
+    })
+  }
   return (
     <View style={styles.container}>
-      <Text>hello! first native app</Text>
+      <Text style={styles.textStyle}>hello! first native app</Text>
+      
+  <View style={styles.list}>{list()}</View>
+   
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,9 +35,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '',
+    flexDirection: "column",
+    backgroundColor: 'white',
     color:"red",
     alignItems: 'center',
     justifyContent: 'center',
+    padding:20
   },
+  textStyle:{
+    fontSize:30,
+    fontWeight:"700",
+    color:"tomato",
+    // margin:-20
+  },
+  list:{
+  color:'white',
+  backgroundColor:'tomato',
+  padding:20,
+  borderRadius:20
+
+  }
 });
